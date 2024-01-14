ac4cb64098a73c0a39f9c783fd47c25fc02cee54
nassima bression <benasima@yahoo.fr>
2024-01-14 13:50:30 +0100

    feat [base de données]: ajout des variables d'environnement pour la sécurisation de la base de données

--- backend/db_config/db_config.js
+++ backend/db_config/db_config.js
@@ -1,19 +1,5 @@
-const PASSWORD = "";
-const USER = "";
 
-if (!PASSWORD) {
-	console.log("*****************************");
-	console.log("* Database password not set *");
-	console.log("*****************************");
-}
-
-if (!USER) {
-	console.log("*****************************");
-	console.log("* Database user not set     *");
-	console.log("*****************************");
-}
-
-const DB_URL = `mongodb+srv://${USER}:${PASSWORD}@cluster0.eiaz3mk.mongodb.net/?retryWrites=true&w=majority`;
+const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`;
 
 const dbConfig = {
 	DB_URL: DB_URL
